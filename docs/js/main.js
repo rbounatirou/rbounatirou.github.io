aa66374edf478fa353c5643c49175824a9417de6
Rodolphe Bounatirou <rodolphe.bounatirou@gmail.com>
2023-10-01 11:10:42 +0200

    Update age

--- docs/js/main.js
+++
@@ -1,8 +0,0 @@
-let buttons = document.querySelectorAll('.navbt');
-buttons.forEach(e => {
-    e.addEventListener('click', ev => {
-        let name = '#' + ev.target.getAttribute('value');
-        let el = document.querySelector(name);
-        el.scrollIntoView();
-    });
-});
